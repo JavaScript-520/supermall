2e86afca26d38adeb13642e89bd76ab45c426bd1
Ouyang Kangzhen <xiaofeng504@qq.com>
2021-05-16 17:42:01 +0800

    编写标签控制组件，并引入到Home主页上使用

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <!--标签控制组件-->
+  <div id="tabControl">
+    <!--获取标签控制的每一个名称-->
+    <div v-for="(item,index) in tabControlItems" @click="clickIndex(index)">
+      <!--确保当前的名称位置与data里的isActiveIndex一样才可以变换位置样式-->
+      <span :class="{isActive:index===isActiveIndex}">{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+//标签控制组件
+export default {
+  name: "TabControl",
+  data() {
+    return {
+      //记录当前的标签名称在哪个下标上
+      isActiveIndex: 0
+    }
+  },
+  props: {
+    //标签控制上的标签名称
+    tabControlItems: {
+      type: Array,
+      require: true
+    }
+  },
+  methods: {
+    //被出发时根据传来的下标设置到本身的data里
+    clickIndex(index) {
+      this.isActiveIndex = index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+#tabControl {
+  height: 40px;
+  display: flex;
+  text-align: center;
+  margin-top:5px;
+  background: #fff;
+
+}
+
+#tabControl > div {
+  flex: 1;
+  font: normal 600 14px/40px "微软雅黑";
+}
+
+#tabControl span {
+  padding: 0 10px 3px;
+}
+
+.isActive {
+  color: #f69;
+  border-bottom: 2px solid #f69;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,10 +8,67 @@
     <HomeRecommend :recommend="recommend"></HomeRecommend>
     <!--导入本周流行组件（首页封装）-->
     <HomePopular :popularList="popularList"></HomePopular>
+    <!--导入标签控制组件到首页上-->
+    <TabControl :tabControlItems="tabControlItems" class="tabCon"></TabControl>
+    <!--临时加上可以看到下滑效果-->
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+      <li>32</li>
+      <li>33</li>
+      <li>34</li>
+      <li>35</li>
+      <li>36</li>
+      <li>37</li>
+      <li>38</li>
+      <li>39</li>
+      <li>40</li>
+      <li>41</li>
+      <li>42</li>
+      <li>43</li>
+      <li>44</li>
+      <li>45</li>
+      <li>46</li>
+      <li>47</li>
+      <li>48</li>
+      <li>49</li>
+      <li>50</li>
+    </ul>
   </div>
 </template>
 
 <script>
+//导入标签控制组件
+import TabControl from "components/content/tabControl/TabControl";
 
 //导入首页拆分封装的头部导航栏
 import HomeNavBar from "views/home/childComps/HomeNavBar";
@@ -38,7 +95,9 @@ export default {
       //存放推荐栏数据
       recommend: [],
       //存放流行数据
-      popularList: []
+      popularList: [],
+      //标签控制上要传入的数据标签
+      tabControlItems: ['流行', '新款', '精选'],
     }
   },
   //组件初始化钩子
@@ -64,10 +123,15 @@ export default {
   },
   //注册组件
   components: {
+    TabControl,
     HomeSwiper, HomeNavBar, HomeRecommend, HomePopular
   }
 }
 </script>
 <style scoped>
-
+/*设置此class其实是设置到TabControl组件上，设置后上滑会定位上指定高度 是相对定位与绝对定位的切换*/
+.tabCon {
+  position: sticky;
+  top: 44px;
+}
 </style>
